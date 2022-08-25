9f55436e696bea1d4675370948f4766b9688a0e2
Serge Labada <sergelabada@gmail.com>
2022-08-25 14:02:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson09jetpacknavigation/FragmentFirst.kt
@@ -0,0 +1,39 @@
+package com.example.lesson09jetpacknavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.lesson09jetpacknavigation.databinding.FragmentFirstBinding
+
+class FragmentFirst : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentFirstBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            button.setOnClickListener {
+                findNavController().navigate(R.id.to_second_fragment)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/lesson09jetpacknavigation/FragmentSecond.kt
@@ -0,0 +1,34 @@
+package com.example.lesson09jetpacknavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.lesson09jetpacknavigation.databinding.FragmentSecondBinding
+
+class FragmentSecond : Fragment() {
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentSecondBinding.inflate(inflater, container, false)
+            .also { _binding = it }
+            .root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
